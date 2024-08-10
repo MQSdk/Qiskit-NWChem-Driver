911f6751d7687e999857a57cd09f496c8790e40f
JasenLiuCoding <410952788@qq.com>
2024-08-10 10:33:19 +0200

    rearrange code file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Qiskit NWchem Driver
 Based on work from https://github.com/dlr-wf/Qiskit-Quantum-Espresso-Driver
+## Instruction to run the demo
+### 1. install package
+```
+pip install -r requirements.txt
+```
+### 2. running nwchem_qiskit_driver.ipynb file to see results 
+
--- nwchem2yaml.py
+++ nwchem2yaml.py
@@ -1,18 +1,4 @@
-
-emitter_yaml = False
-emitter_ruamel = False
-
 import sys
-try:
-    try:
-        import ruamel.yaml as ruamel
-    except ImportError:
-        import ruamel_yaml as ruamel
-    emitter_ruamel = True
-except ImportError:
-    import yaml
-    emitter_yaml = True
-    
 
 preamble="""
 "$schema": https://raw.githubusercontent.com/Microsoft/Quantum/master/Chemistry/Schema/broombridge-0.1.schema.json
@@ -31,8 +17,7 @@ def is_float(str):
         return True
     except ValueError:
         return False
-
-def extract_fields():
+def extract_fields(file):
     data = {} #yaml.load(initial_input)
     data['format'] = {'version' : '0.1'}
     data['bibliography'] = [{'url' : 'https://nwchemgit.github.io'}]
@@ -41,22 +26,23 @@ def extract_fields():
     skip_input_geometry = False
     geometry = None
     coulomb_repulsion = None
-    scf_energy = None
-    scf_energy_offset = None
-    energy_offset = None
+    # scf_energy = None
+    # scf_energy_offset = None
+    # energy_offset = None
     one_electron_integrals = None
     two_electron_integrals = None
-    n_electrons_alpha = None
+    # n_electrons_alpha = None
     basis_set = None
-    n_electrons_beta = None
+    # n_electrons_beta = None
     n_orbitals = None
     initial_state = None
-    ccsd_energy = None
+    # ccsd_energy = None
     reader_mode = ""
-    excited_state_count = 1
-    excitation_energy = 0.0
-    fci_energy = None
-    for line in sys.stdin.readlines():
+    # excited_state_count = 1
+    # excitation_energy = 0.0
+    # fci_energy = None
+    f = open(file)
+    for line in f.readlines():
         ln = line.strip()
         ln_segments = ln.split()
         if len(ln) == 0 or ln[0]=="#": #blank or comment line
@@ -68,20 +54,6 @@ def extract_fields():
                 coulomb_repulsion = {
                     'units' : 'hartree',
                     'value' : float(ln_segments[2])
-                }
-            elif ln_segments[:2] == ["EHF(total)", "="]:
-                scf_energy = {
-                    'units' : 'hartree',
-                    'value' : float(ln_segments[2])
-                }
-            elif ln_segments[:3] == ["Shift", "(HFtot-HFA)", "="]:
-                scf_energy_offset = {
-                    'units' : 'hartree',
-                    'value' : float(ln_segments[3])
-                }
-                energy_offset = {
-                    'units' : 'hartree',
-                    'value' : float(ln_segments[3])
                 }
             elif ln == "begin_one_electron_integrals":
                 reader_mode = "one_electron_integrals"
@@ -98,46 +70,23 @@ def extract_fields():
                     'index_convention' : 'mulliken',
                     'values' : []
                 }
-            elif ln.find("Number of active alpha electrons") != -1:
-                n_electrons_alpha = int(ln_segments[-1])
-            elif ln.find("Number of active beta electrons") != -1:
-                n_electrons_beta = int(ln_segments[-1])
+            elif ln.find("number of electrons") != -1 and ln.find("Fourier space") != -1:
+                try:
+                    n_electrons = int(ln_segments[5]) + int(ln_segments[11])
+                except:
+                    continue
+            elif ln.find("number of orbitals") != -1 and ln.find("Fourier space") != -1:
+                n_orbitals = int(ln_segments[6]) + int(ln_segments[12])
             elif ln.find("Number of active orbitals") != -1:
                 n_orbitals = int(ln_segments[-1])
+            elif ln.find("ion-ion   energy") != -1:
+                coulomb_repulsion = {
+                    'units' : 'hartree',
+                    'value' : float(ln_segments[3])
+                }
             elif ln.find("Total MCSCF energy") != -1:
                 fci_val = float(ln.split("=")[1].strip())
                 fci_energy = {"units" : "hartree", "value" : fci_val, "upper": fci_val+0.1, "lower":fci_val-0.1}
-            #elif ln.split("=")[0].strip() == "CCSD total energy / hartree":
-            #    if fci_energy is None:
-            #        fci_val = float(ln.split("=")[1].strip())
-            #        fci_energy = {"units" : "hartree", "value" : fci_val, "upper": fci_val*0.99, "lower":fci_val*1.01}
-            elif ln == "Ground state specification:":
-                reader_mode = "initial_state"
-                assert ccsd_energy is not None, "CCSD energy should be available before the groud state specification."
-                if initial_state is None:
-                    initial_state = []
-                initial_state += [ {'state':{
-                        'label' : '|G>',
-                        'energy' : { 'units' : 'hartree', 
-                                     'value' : ccsd_energy} ,
-                        'superposition' : []
-                        }}]
-            elif ln == "Excited state specification:":
-                reader_mode = "initial_state"
-                assert ccsd_energy is not None, "CCSD energy should be available before the excited state specification."
-                if initial_state is None:
-                    initial_state = []
-                initial_state += [ {'state':{
-                        'label' : '|E%d>'%(excited_state_count),
-                        'energy' : { 'units' : 'hartree', 
-                                     'value' : ccsd_energy+excitation_energy} ,
-                        'superposition' : []
-                        }}]
-                excited_state_count += 1
-            elif ln.split("=")[0].strip() == "Excitation energy / hartree":
-                excitation_energy = float(ln.split("=")[1].strip())
-            elif ln.split("=")[0].strip() == "CCSD total energy / hartree":
-                ccsd_energy = float(ln.split("=")[1].strip())
             elif ln == '''Geometry "geometry" -> ""''':
                 reader_mode = "cartesian_geometry"
                 if geometry is None:
@@ -186,11 +135,6 @@ def extract_fields():
                 assert len(ln_segments) >= 3
                 geometry = {'coordinate_system': 'cartesian'}
                 geometry['units'] = ln_segments[2]
-            elif ln_segments[0].lower() == "basis":
-                reader_mode = "input_basis"
-                basis_set = {}
-                basis_set['name'] = 'unknown'
-                basis_set['type'] = 'gaussian'
         elif reader_mode=="input_geometry":
             if ln.lower()=="end":
                 reader_mode = "input_deck"
@@ -238,62 +182,20 @@ def extract_fields():
                       int(ln_segments[3]),
                       float(ln_segments[4])
                     ]]
-    if fci_energy is None:
-        assert ccsd_energy is not None
-        fci_energy = {"units" : "hartree", "value" : ccsd_energy, "upper": ccsd_energy+0.1, "lower": ccsd_energy-0.1}
-
     assert one_electron_integrals is not None, "one_electron_integrals is missing from NWChem output. Required to extract YAML"
     assert two_electron_integrals is not None, "two_electron_integrals is missing from NWChem output. Required to extract YAML"
     assert geometry is not None, "geometry information is missing from NWChem output. Required to extract YAML"
-    assert basis_set  is not None, "basis_set is missing from NWChem output. Required to extract YAML"
-    assert coulomb_repulsion  is not None, "coulomb_repulsion is missing from NWChem output. Required to extract YAML"
-    assert scf_energy  is not None, "scf_energy is missing from NWChem output. Required to extract YAML"
-    assert scf_energy_offset  is not None, "scf_energy_offset is missing from NWChem output. Required to extract YAML"
-    assert energy_offset  is not None, "energy_offset is missing from NWChem output. Required to extract YAML"
-    assert fci_energy  is not None, "fci_energy is missing from NWChem output. Required to extract YAML"
     assert n_orbitals  is not None, "n_orbitals is missing from NWChem output. Required to extract YAML"
-    assert n_electrons_alpha  is not None, "n_electrons_alpha is missing from NWChem output. Required to extract YAML"
-    assert n_electrons_beta  is not None, "n_electrons_beta is missing from NWChem output. Required to extract YAML"
     hamiltonian = {'one_electron_integrals' : one_electron_integrals,
                    'two_electron_integrals' : two_electron_integrals}
     integral_sets =  [{"metadata": { 'molecule_name' : 'unknown'},
                        "geometry":geometry,
-                       "basis_set":basis_set,
                        "coulomb_repulsion" : coulomb_repulsion,
-                       "scf_energy" : scf_energy,
-                       "scf_energy_offset" : scf_energy_offset,
-                       "energy_offset" : energy_offset,
-                       "fci_energy" : fci_energy,
                        "hamiltonian" : hamiltonian,
                        "n_orbitals" : n_orbitals,
-                       "n_electrons" : n_electrons_alpha + n_electrons_beta }]
+                       "n_electrons" : n_electrons }]
     if initial_state is not None:
         integral_sets[-1]["initial_state_suggestions"] = initial_state
     data['integral_sets'] = integral_sets
+    f.close()
     return data
-    
-def emitter_ruamel_func():
-    yaml = ruamel.YAML(typ="safe")
-    #yaml.default_flow_style = False
-    print(preamble)
-    data = extract_fields()
-    yaml.dump(data, sys.stdout)
-
-def emitter_yaml_func():
-    print(preamble)
-    data = extract_fields()
-    print(yaml.dump(data, default_flow_style=False))
-
-    
-
-def main():
-    assert emitter_yaml or emitter_ruamel, "Extraction failed: could not import YAML or RUAMEL packages."
-    if emitter_yaml:
-        emitter_yaml_func()
-    elif emitter_ruamel:
-        emitter_ruamel_func()
-    else:
-        assert False, "Unreachable code"
-
-if __name__ == "__main__":
-    main()
